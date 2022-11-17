e7549fd1c7636c6e5ca36698251272a3f12a1d7c
Ivan Mikhailov <meehighlov@Ivans-MacBook-Air.local>
2022-11-18 00:54:07 +0300

    add auth decorator

--- auth.py
+++ auth.py
@@ -1,3 +1,9 @@
+from functools import wraps
+
+from telegram import Update
+from telegram.ext import  CallbackContext
+
+from config import config
 from database.ext.users import get_user_by_id
 
 
@@ -5,3 +11,23 @@ def is_auth_user(user_id: int) -> bool:
     user = get_user_by_id(user_id)
     return user is not None
 
+
+def auth(set_user=False):
+
+    def auth_handling(handler):
+        def check_username(update: Update, context: CallbackContext, *args, **kwargs):
+
+            user_id = update.message.from_user.id
+            user = get_user_by_id(user_id)
+
+            if not user:
+                return
+
+            if set_user:
+                return handler(update, context, user=user)
+
+            return handler(update, context)
+
+        return check_username
+
+    return auth_handling
