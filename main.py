3757ff8550c9e45cf970676e331b49a872262a72
Ivan.V.Eliseev <AdesWepore73@gmail.com>
2022-12-27 18:07:32 +0300

    add delete handler in main.py

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ from app.handlers.code import handle as code_command_handler
 from app.handlers.me import handle as me_command_handler
 from app.handlers.setme import handle as setme_command_handler
 from app.handlers.chat import handle as chat_command_handler
+from app.handlers.delete import handle as delete_command_handler
 
 
 logger = logging.getLogger(__name__)
@@ -34,6 +35,7 @@ def main() -> None:
     dispatcher.add_handler(CommandHandler('help', help_command_handler))
     dispatcher.add_handler(CommandHandler('me', me_command_handler))
     dispatcher.add_handler(CommandHandler('chat', chat_command_handler))
+    dispatcher.add_handler(CommandHandler('del', delete_command_handler))
     dispatcher.add_handler(setme_command_handler)
     dispatcher.add_handler(code_command_handler)
 
